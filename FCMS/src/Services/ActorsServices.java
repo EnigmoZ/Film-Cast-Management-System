943daa10721358c94805f5315306a22e115e14e8
Arpit <javadevarpit@gmail.com>
2024-05-31 10:46:25 +0530

    8 class added with inside code

--- FCMS/src/Services/ActorsServices.java
+++ FCMS/src/Services/ActorsServices.java
@@ -1,5 +1,41 @@
 package Services;
+import java.sql.SQLException;
+import java.util.List;
+
+import DAO.ActorsDAO;
+import Models.Actors;
 
 public class ActorsServices {
+    private ActorsDAO actorDAO;
+
+    public ActorsServices() {
+        this.actorDAO = new ActorsDAO();
+    }
+
+    public void addActor(Actors actor) throws SQLException {
+        actorDAO.addActor(actor);
+    }
+
+    public List<Actors> getAllActors() throws SQLException {
+        return actorDAO.getAllActors();
+    }
+
+    public Actors searchActorByName(String name) throws SQLException {
+        return actorDAO.searchActorByName(name);
+    }
+
+    public List<Actors> getActorsByNationality(String nationality) throws SQLException {
+        return actorDAO.getActorsByNationality(nationality);
+    }
+
+    public void updateActorNationality(int actorId, String nationality) throws SQLException {
+        actorDAO.updateActorNationality(actorId, nationality);
+    }
+
+    public void deleteActors(int actorId) throws SQLException {
+        actorDAO.deleteActors(actorId);
+    }
+
+
 
 }
--- FCMS/src/Services/ReviewsServices.java
+++ FCMS/src/Services/ReviewsServices.java
@@ -1,4 +1,34 @@
 package Services;
+import DAO.ReviewsDAO;
+import Models.Reviews;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class ReviewsServices {
+    private ReviewsDAO reviewDAO;
+
+    public ReviewsServices() {
+        this.reviewDAO = new ReviewsDAO();
+    }
+
+    public void addReview(Reviews review) throws SQLException {
+        reviewDAO.addReview(review);
+    }
+
+    public List<Reviews> getAllReviews() throws SQLException {
+        return reviewDAO.getAllReviews();
+    }
+
+    public Reviews searchReviewById(int id) throws SQLException {
+        return reviewDAO.searchReviewById(id);
+    }
+
+    public void updateReview(Reviews review) throws SQLException {
+        reviewDAO.updateReview(review);
+    }
+
+    public void deleteReview(int reviewId) throws SQLException {
+        reviewDAO.deleteReview(reviewId);
+    }
 }
